c56db85c6af17a9a98e083121082d72fd8ae404c
KunalBari <106544507+KunalBari26@users.noreply.github.com>
2024-11-09 11:25:51 +0530

    Create q_1_animation.m

---
+++ q_1_animation.m
@@ -0,0 +1,87 @@
+clc;
+clear;
+
+
+M = 1;          
+m = 0.5;         
+k = 1;           
+
+omega1 = 0;                              
+omega2 = sqrt(k / M);                    
+omega3 = sqrt(k * (1 + 2 * M / m) / M);  
+
+
+c1 = 0.0; 
+c2 = 0.05;  
+c3 = 0.03; 
+
+
+numFrames = 200;                               
+t = linspace(0, 2 * pi / min(omega2, omega3), numFrames);  
+
+
+figure;
+axis equal;
+axis([-0.3 0.3 -0.2 0.2]); 
+hold on;
+grid on;
+title('CO2 Molecule Oscillations');
+xlabel('Position');
+ylabel('Relative Displacement');
+
+
+colors = ['r', 'g', 'b']; 
+labels = {'O', 'C', 'O'};
+
+
+videoFilename = 'Question1 Final Video.mp4';
+video = VideoWriter(videoFilename, 'MPEG-4');
+video.FrameRate = 20;
+open(video);
+
+
+for i = 1:numFrames
+    clf; 
+    axis equal;
+    axis([-0.3 0.3 -0.2 0.2]);
+    hold on;
+    title('CO2 Molecule Oscillations');
+    xlabel('Position');
+    ylabel('Relative Displacement');
+    grid on;
+    
+    
+    x1 = c1 * cos(omega1 * t(i)) + c2 * cos(omega2 * t(i)) + c3 * cos(omega3 * t(i)); 
+    x2 = c1 * cos(omega1 * t(i)) - (2 * M / m) * c3 * cos(omega3 * t(i));            
+    x3 = -c1 * cos(omega1 * t(i))  + c3 * cos(omega3 * t(i)); 
+
+    
+    if x1 > x2
+        x1 = x2 - 0.1; 
+    end
+    if x3 < x2
+        x3 = x2 + 0.1; 
+    end
+
+    
+    plot(x1,0, 'o', 'MarkerSize', 20, 'MarkerFaceColor', colors(1));
+    plot(x2,0, 'o', 'MarkerSize', 20, 'MarkerFaceColor', colors(2));
+    plot(x3,0, 'o', 'MarkerSize', 20, 'MarkerFaceColor', colors(1));
+    text(x1, 0.05, labels{1}, 'HorizontalAlignment', 'center', 'Color', colors(1));
+    text(x2, 0.05, labels{2}, 'HorizontalAlignment', 'center', 'Color', colors(2));
+    text(x3, 0.05, labels{3}, 'HorizontalAlignment', 'center', 'Color', colors(1));
+
+    
+    plot([x1, x2], [0, 0], 'k-', 'LineWidth', 2); 
+    plot([x2, x3], [0, 0], 'k-', 'LineWidth', 2); 
+
+    
+    frame = getframe(gcf);
+    writeVideo(video, frame);
+
+    pause(0.05); 
+end
+
+
+close(video);
+disp(['Video saved as ', videoFilename]);
